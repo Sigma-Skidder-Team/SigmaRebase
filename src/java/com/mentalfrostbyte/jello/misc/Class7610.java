0040548bde3df538f1a5accb64d89a04baebc07f
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-18 17:02:17 -0600

    chore: moar classes

---
+++ src/java/com/mentalfrostbyte/jello/misc/Class7610.java
@@ -0,0 +1,9 @@
+package com.mentalfrostbyte.jello.misc;
+
+
+import net.minecraft.tags.ITag;
+import net.minecraft.util.ResourceLocation;
+
+public interface Class7610<T> extends ITag<T> {
+    ResourceLocation getName();
+}
