94332fed576897f37ac4a7f4e2ba2fc772bcdc71
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-01-18 14:54:34 -0600

    chore: remove MCP stuff & move stuff into `src/main/java`

--- src/java/net/minecraft/client/gui/IRenderable.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.client.gui;
-
-import com.mojang.blaze3d.matrix.MatrixStack;
-
-public interface IRenderable
-{
-    void render(MatrixStack matrices, int mouseX, int mouseY, float delta);
-}
