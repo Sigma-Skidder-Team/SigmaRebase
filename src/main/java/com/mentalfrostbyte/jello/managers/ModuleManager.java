fe919e2c898cac0945aec934e33d80b92193b803
rmpb <mark@wonderland.ac>
2026-03-07 17:03:08 +0200

    chore: remove NewAura

--- src/main/java/com/mentalfrostbyte/jello/managers/ModuleManager.java
+++ src/main/java/com/mentalfrostbyte/jello/managers/ModuleManager.java
@@ -93,7 +93,6 @@ public class ModuleManager {
         this.register(new WTap());
         this.register(new BowAimbot());
         this.register(new InfiniteAura());
-        this.register(new NewAura());
         // RENDER
         this.register(new AntiBlind());
         this.register(new DVDSimulator());
